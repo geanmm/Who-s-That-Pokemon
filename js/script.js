c23c72b82d1014d92c00d909631046e703234e30
Gean Moura <geanmoura1511@gmail.com>
2023-01-17 01:23:34 -0300

    added timer and score

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const time_line = document.querySelector("#time_line");
+const timeText = document.querySelector("#time_left_txt");
+const timeCount = document.querySelector("#timer_sec");
+const next_btn = document.querySelector("#btn_next");
+const start_btn = document.querySelector("#start_btn");
+const try_again = document.querySelector("#try_again");
+let options = [];
+let userScore = 0;
+let timeValue = 15;
+let counter;
+let counterLine;
+let widthValue = 0;
+
 function shuffleArray(array = []) {
   var actual = array.length,
     temp,
@@ -28,7 +41,6 @@ async function getPokemons() {
   const pokemons = response.results;
 
   const currentPokemon = getRandomPokemon(pokemons);
-  const options = [];
 
   options.push(currentPokemon);
 
@@ -60,22 +72,87 @@ async function renderPokemonAndOptions(options, currentPokemon) {
   const buttons = document.querySelectorAll("#answ_options li");
   buttons.forEach((item) => {
     item.addEventListener("click", (e) => {
+      clearInterval(counter);
+      clearInterval(counterLine);
+
       const buttonSelected = e.target;
-      if (buttonSelected.innerText === currentPokemon.name) {
+
+      if (buttonSelected.innerText.toLowerCase() === currentPokemon.name) {
+        userScore += 1;
         document.querySelector("#pokemon_pic").classList.remove("notSolved");
         document.querySelector(
           "#pokemon_info"
         ).innerText = `${response.id} - ${response.name}`;
-
-        const nextBtn = document.querySelector("#btn_next");
-
-        nextBtn.classList.remove("hidden");
-        nextBtn.addEventListener("click", () => {
-          window.location.reload();
-        });
+        document.querySelector("#btn_next").classList.remove("hidden");
+      } else {
+        setTimeout(() => {
+          document.querySelector("#game").classList.add("hidden");
+          document.querySelector("#gameover").classList.remove("hidden");
+        }, 2000);
       }
+      document.querySelector("#user_score").innerHTML = userScore;
+      document.querySelector("#user_finalscore").innerHTML = userScore;
+      document.querySelector("#answ_options").classList.add("disable_listener");
     });
   });
+}
+try_again.addEventListener("click", () => {
+  window.location.reload();
+});
+start_btn.addEventListener("click", () => {
+  document.querySelector("#game").classList.remove("hidden");
+  start_btn.classList.add("hidden");
+  document.querySelector("#start").classList.add("hidden");
+  startTimer(15);
+  startTimerLine(0);
+});
+next_btn.addEventListener("click", () => {
+  options = [];
+  const pokemonList = document.querySelector("#answ_options");
+  pokemonList.innerHTML -= `<li></li>`;
+  if ((pokemonList.innerHTML = "NaN")) {
+    pokemonList.innerHTML = "";
+  }
+  document.querySelector("#pokemon_info").innerText = "# - Pokemon Name";
+  next_btn.classList.add("hidden");
+  document.querySelector("#pokemon_pic").classList.add("notSolved");
+  getPokemons();
+  startTimer(15);
+  startTimerLine(0);
+
+  document.querySelector("#answ_options").classList.remove("disable_listener");
+});
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time;
+    time--;
+
+    if (time < 9) {
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero;
+    }
+    if (time < 0) {
+      clearInterval(counter);
+      timeText.textContent = "Time Off";
+      setTimeout(() => {
+        document.querySelector("#game").classList.add("hidden");
+        document.querySelector("#gameover").classList.remove("hidden");
+      }, 2000);
+    }
+  }
+}
+function startTimerLine(time) {
+  counterLine = setInterval(timer, 27);
+  function timer() {
+    time += 1; //upgrading time value with 1
+    time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+    if (time > 599) {
+      //if time value is greater than 549
+      clearInterval(counterLine); //clear counterLine
+    }
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
